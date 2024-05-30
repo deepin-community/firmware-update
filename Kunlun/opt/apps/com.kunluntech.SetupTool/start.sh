f62f9d2859cb9960a2121e3915bf32f8533eb854
gli@kunluntech.com.cn <gli@kunluntech.com.cn>
2024-05-30 09:22:29 +0800

    删除运行时需要的库文件，并在control中添加依赖项

--- Kunlun/opt/apps/com.kunluntech.SetupTool/start.sh
+++ Kunlun/opt/apps/com.kunluntech.SetupTool/start.sh
@@ -4,8 +4,6 @@ DIR="$(dirname "$0")"
 src="$(pwd)"
 cd "$DIR"
 
-"$(pwd)/patchelf" --set-interpreter "$(pwd)/lib/ld.so.1" "$(pwd)/UiComp"
-
 pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY "$(pwd)/UiComp"
 
 cd "$src"
